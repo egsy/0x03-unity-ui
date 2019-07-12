da29ab9f44bc4589f2955304004f5810f29e0500
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2019-07-11 21:42:56 -0500

    Add coin animation

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Animates coins in maze
+/// </summary>
+public class Rotator : MonoBehaviour
+{
+    public float rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotate = 45.0f;
+        transform.Rotate(rotate * Time.deltaTime, 0, 0);
+    }
+}
