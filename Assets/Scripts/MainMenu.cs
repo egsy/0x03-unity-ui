bf802ad2526f50dc42c468cda92bd791a07caeb1
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2019-07-18 21:46:04 -0500

    Add accessibility options

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    /// <summary>
+    /// 
+    /// </summary>
     public void PlayMaze()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (colorblindMode == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public void QuitMaze()
     {
         Debug.Log("Quit Game");
